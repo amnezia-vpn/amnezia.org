8f4704fc7a7dde7affce271c1288dc510a1bb8bc
pokamest <pokamest@gmail.com>
2021-01-11 22:11:02 +0300

    deploy fix

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 function ssh_cmd () {
-  ssh -o LogLevel=error $REMOTE_USER:$REMOTE_PASSWORD@$REMOTE_IP "$1"
+  ssh -o LogLevel=error -o StrictHostKeyChecking=no $REMOTE_USER:$REMOTE_PASSWORD@$REMOTE_IP "$1"
 }
 
 REMOTE_PATH="/var/www/AMNEZIA.ORG"
@@ -10,3 +10,4 @@ echo "===> Deploy"
 ssh_cmd "cd $REMOTE_PATH && wget https://github.com/amnezia-vpn/amnezia.org/archive/master.zip"
 ssh_cmd "unzip $REMOTE_PATH/master.zip -d $REMOTE_PATH"
 ssh_smd "mv -f $REMOTE_PATH/amnezia.org*/docs/* $REMOTE_PATH/ && rm $REMOTE_PATH/master.zip"
+ssh_smd certbot -q renew
