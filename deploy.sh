7bd0126fa0df2a329874607f4a6bda127833f16a
pokamest <pokamest@gmail.com>
2021-01-11 22:30:20 +0300

    deploy fix

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,5 @@ ssh_cmd "rm -rf $REMOTE_PATH/*"
 echo "===> Deploy"
 ssh_cmd "cd $REMOTE_PATH && wget https://github.com/amnezia-vpn/amnezia.org/archive/master.zip"
 ssh_cmd "unzip $REMOTE_PATH/master.zip -d $REMOTE_PATH"
-ssh_smd "mv -f $REMOTE_PATH/amnezia.org*/docs/* $REMOTE_PATH/ && rm $REMOTE_PATH/master.zip"
-ssh_smd certbot -q renew
+ssh_cmd "mv -f $REMOTE_PATH/amnezia.org*/docs/* $REMOTE_PATH/ && rm $REMOTE_PATH/master.zip"
+ssh_cmd "certbot -q renew"
